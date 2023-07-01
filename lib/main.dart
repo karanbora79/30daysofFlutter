64ed91ddf6bb6d9483e5763898c4d76540a8efa4
Karan <karan.official79@gmail.com>
2023-07-01 19:53:21 +0530

    Day 2 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalogue/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,14 +10,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // double pi = 3.14;
+    // bool isMale = true;
+    // num temp = 30.5; // takes both
+
+    // var day = "tuesday"; // takes any value
+    // const pi2 = 3.14;
+    // final vs const..... if final have any list the we can add something to that list
+    // $ string interpolation
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: const Text("Welcome to 30 days of flutter"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
